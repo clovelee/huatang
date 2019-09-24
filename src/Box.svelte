d33743f25cead2affe526f10feb424bbbb72941f
Moon Snow <w.shangli@icloud.com>
2019-09-24 22:43:18 +0800

    show box

--- src/Box.svelte
+++ src/Box.svelte
@@ -29,14 +29,34 @@
 </script>
 
 <style>
+ ul {padding-inline-start:0;}
+
   .box {
     font-family: Verdana, Geneva, Tahoma, sans-serif;
-    background-color: #ffbfcb;
+    background-color: #fff;
     padding: 2em;
     opacity: 0.8;
     color: #000;
     display: block;
   }
+	.box li {
+		list-style: none;
+		line-height: 2em;
+	}
+	.prt-index {
+		font-size: 12px;
+		color: #666;
+		padding-right:1em;
+	}
+	.prt-name {
+		font-size: 14px;
+		font-weight: 500;
+		padding-right:1em;
+	}
+	.prt-price {
+		font-size: 14px;
+		color: brown;
+	}
 
 </style>
 
@@ -45,7 +65,10 @@
   <ul>
     {#each products as item, i}
       <!-- content here -->
-      <li>{i} {item.product} ¥{item.price}</li>
+      <li>
+				<span class="prt-index">{i}</span>
+				<span class="prt-name">{item.product}</span>
+				<span class="prt-price">¥{item.price}</span></li>
     {/each}
   </ul>
 </div>
--- src/Category.svelte
+++ src/Category.svelte
@@ -2,8 +2,14 @@
   export let categories = [];
 </script>
 
+<style>
+ul {padding-inline-start:0;}
+ul li {list-style: none;font-size: 12px;display: inline-block;padding-right: 4px;}
+button {border-radius: 20%;}
+</style>
+
 <div class="box">
-  <h3>选择产品类别</h3>
+  <h4>选择产品类别</h4>
   <ul>
     {#each categories as item, i}
       <!-- content here -->
